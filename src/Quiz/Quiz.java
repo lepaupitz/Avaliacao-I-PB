63a56171f41372bdfa345eea97f96383451e805c
Leandro Paupitz <lepaupitz2@gmail.com>
2023-12-26 16:08:59 -0300

    Quiz - Commit 2
    Adicionado arrays, sistema de validação das perguntas se estão certas ou erradas

--- src/Quiz/Quiz.java
+++ src/Quiz/Quiz.java
@@ -4,11 +4,41 @@ public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
 
+    // Perguntas
+    String[] perguntas = {
+            "teste?\n a) 1",
+            "teste1?"
+    };
+
+    // Respostas
+    String[] respostas = {
+            "1",
+            "teste"
+    };
 
     //Nome do usuário
     System.out.print("Digite seu nome: ");
     String nomeUsuario = scanner.nextLine();
     System.out.println("Usuário: " + nomeUsuario);
+    System.out.println("Responda as seguintes pergutas: ");
+
+    boolean[] resultados = new boolean[perguntas.length];
+
+    for (int i = 0; i < perguntas.length; i++) {
+        System.out.println(perguntas[i]);
+        System.out.print("Resposta: ");
+        String respostaUsuario = scanner.nextLine();
+
+        if (respostaUsuario.equalsIgnoreCase(respostas[i])) {
+            resultados[i] = true;
+            System.out.println("Resposta correta!");
+        }
+        else {
+            resultados[i] = false;
+            System.out.println("Resposta errada.");
+
+        }
+    }
 
 }
 
