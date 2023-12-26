61d1fc5bc985e0c05be5959876e33f3623ff9569
Leandro Paupitz <lepaupitz2@gmail.com>
2023-12-26 16:51:01 -0300

    Quiz - Commit 3
    Adicionado as perguntas, contador de erros e acertos, e adicionado o resultado final

--- src/Quiz/Quiz.java
+++ src/Quiz/Quiz.java
@@ -6,24 +6,39 @@ public static void main(String[] args) {
 
     // Perguntas
     String[] perguntas = {
-            "teste?\n a) 1",
-            "teste1?"
+            "Quanto vale a divisão de 10 por 2?",
+            "Qual é a soma de 10 com 20?",
+            "Quantos pés tem uma mesa comum?",
+            "Resolva a equação: x = 2 - 1\nQual o valor de x?",
+            "Quantas patas tem um cachorro?",
+            "Quantas patas tem um gato?",
+            "Quantos lados tem um cubo?",
+            "Qual o número de arestas de um círculo?"
     };
 
     // Respostas
     String[] respostas = {
+            "5",
+            "30",
+            "4",
             "1",
-            "teste"
+            "4",
+            "4",
+            "6",
+            "0",
     };
+
+    int errado = 0;
+    int correto = 0;
 
     //Nome do usuário
     System.out.print("Digite seu nome: ");
     String nomeUsuario = scanner.nextLine();
-    System.out.println("Usuário: " + nomeUsuario);
-    System.out.println("Responda as seguintes pergutas: ");
+        System.out.println("Responda as seguintes pergutas: ");
 
     boolean[] resultados = new boolean[perguntas.length];
 
+    // Perguntas e verificar se a resposta esta certa ou errada
     for (int i = 0; i < perguntas.length; i++) {
         System.out.println(perguntas[i]);
         System.out.print("Resposta: ");
@@ -31,15 +46,20 @@ public static void main(String[] args) {
 
         if (respostaUsuario.equalsIgnoreCase(respostas[i])) {
             resultados[i] = true;
-            System.out.println("Resposta correta!");
+            System.out.println("Resposta correta!\n");
+            correto++;
         }
         else {
             resultados[i] = false;
-            System.out.println("Resposta errada.");
-
+            System.out.println("Resposta errada.\n");
+            errado++;
         }
     }
 
+    // Resultado Final
+    System.out.println("\nUsuário: " + nomeUsuario);
+    System.out.println("Acertos: " + correto);
+    System.out.println("Erros: " + errado);
+
+    scanner.close();
 }
-
-
