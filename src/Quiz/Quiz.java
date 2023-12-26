0a1cf3b9a9bcbe5ec284a3edf4a922281aa7a6f8
Leandro Paupitz <lepaupitz2@gmail.com>
2023-12-26 16:00:19 -0300

    primeiro commit

---
+++ src/Quiz/Quiz.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+
+    //Nome do usuário
+    System.out.print("Digite seu nome: ");
+    String nomeUsuario = scanner.nextLine();
+    System.out.println("Usuário: " + nomeUsuario);
+
+}
+
+
