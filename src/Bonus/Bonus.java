ce40f5a1176861f1c722992c800f86707d862555
Leandro Paupitz <lepaupitz2@gmail.com>
2023-12-28 17:21:03 -0300

    Bonus - Commit 2
    Adicionado o cálculo do bonus dos funcionários

--- src/Bonus/Bonus.java
+++ src/Bonus/Bonus.java
@@ -10,6 +10,7 @@ public static void main (String[] args) {
     String[] nomeFuncionario = new String[numeroFuncionarios];
     double [] salario = new double[numeroFuncionarios];
 
+
     for (int i  = 0; i < numeroFuncionarios; i++) {
         System.out.print("\nNome do funcionário: " );
         nomeFuncionario[i] = scanner.next();
@@ -18,10 +19,29 @@ public static void main (String[] args) {
         salario[i] = scanner.nextDouble();
     }
 
+
     for (int i = 0; i < numeroFuncionarios; i++) {
         System.out.println("\nFuncionário: " + nomeFuncionario[i]);
         System.out.println("Salário: " + salario[i]);
 
+        if (salario[i] <= 1000) {
+            double bonus = salario[i] * 0.2;
+            double salarioTotal = salario[i] + bonus;
+            System.out.println("Bônus: " + bonus);
+            System.out.println("\nSalário Líquido: " + salarioTotal);
+        } else if (salario[i] > 1000 && salario[i] <  2000) {
+            double bonus = salario[i] * 0.1;
+            double salarioTotal = salario[i] + bonus;
+            System.out.println("Bônus: " + bonus);
+            System.out.println("\nSalário Líquido: " + salarioTotal);
+        }
+        else {
+            double bonus = salario[i] * 0.1;
+            double salarioTotal = salario[i] - bonus;
+            System.out.println("Bônus: " + bonus);
+            System.out.println("\nSalário Líquido: " + salarioTotal);
+        }
     }
 
 }
+
--- src/Bonus/readme.txt
+++ src/Bonus/readme.txt
@@ -2,3 +2,4 @@ Exercício 3 - Bonus
 
 Para armazenar o nome e o salário dos funcionários, pensei de modo em fazer 2 arrays separados, de modo que depois quando for calculado o aumento ou desconto utlize apenas o salário.
 Utilizei um for para para ir armazenando os dados na array, até o número de funcionários desejados para cadastrar.
+Para calcular o bônus eu já delimitei em um for conforme for o salário do funcionário para depois calcular o bônus que ele teria.
