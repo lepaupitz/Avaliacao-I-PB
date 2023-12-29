5da39b9a4d4191169eb2e73a157e039b46bed40a
Leandro Paupitz <lepaupitz2@gmail.com>
2023-12-29 15:15:33 -0300

    Bonus - Commit 3
    Adicionado o tratamento de exceções, e também mudança no cálculo do bonus

--- src/Bonus/Bonus.java
+++ src/Bonus/Bonus.java
@@ -1,47 +1,68 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public static void main (String[] args) {
 
+public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Quantidade de funcionários: ");
-    int numeroFuncionarios = scanner.nextInt();
+    try {
+        int numeroFuncionarios = 0;
 
-    String[] nomeFuncionario = new String[numeroFuncionarios];
-    double [] salario = new double[numeroFuncionarios];
+        while (true) {
+            try {
+                System.out.println("Quantidade de funcionários: ");
+                numeroFuncionarios = scanner.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Por favor, insira um número inteiro válido.");
+                scanner.next();
+            }
+        }
 
+        String[] nomeFuncionario = new String[numeroFuncionarios];
+        double[] salario = new double[numeroFuncionarios];
 
-    for (int i  = 0; i < numeroFuncionarios; i++) {
-        System.out.print("\nNome do funcionário: " );
-        nomeFuncionario[i] = scanner.next();
+        for (int i = 0; i < numeroFuncionarios; i++) {
+            System.out.print("\nNome do funcionário: ");
+            nomeFuncionario[i] = scanner.next();
 
-        System.out.print("Salário do funcionário: ");
-        salario[i] = scanner.nextDouble();
-    }
+            while (true) {
+                    try {
+                        System.out.print("Salário do funcionário: ");
+                        salario[i] = scanner.nextDouble();
+                        break;
+                    } catch (InputMismatchException e) {
+                        System.out.println("Por favor, insira um número válido para o salário.");
+                        scanner.next();
+                    }
+                }
+            }
 
+        for (int i = 0; i < numeroFuncionarios; i++) {
+            System.out.println("\nFuncionário: " + nomeFuncionario[i]);
+            System.out.println("Salário: " + salario[i]);
 
-    for (int i = 0; i < numeroFuncionarios; i++) {
-        System.out.println("\nFuncionário: " + nomeFuncionario[i]);
-        System.out.println("Salário: " + salario[i]);
+            double bonus = 0.0;
+            double salarioTotal = 0.0;
 
-        if (salario[i] <= 1000) {
-            double bonus = salario[i] * 0.2;
-            double salarioTotal = salario[i] + bonus;
-            System.out.println("Bônus: " + bonus);
-            System.out.println("\nSalário Líquido: " + salarioTotal);
-        } else if (salario[i] > 1000 && salario[i] <  2000) {
-            double bonus = salario[i] * 0.1;
-            double salarioTotal = salario[i] + bonus;
+            if (salario[i] <= 1000) {
+                bonus = salario[i] * 0.2;
+                salarioTotal = salario[i] + bonus;
+            } else if (salario[i] > 1000 && salario[i] < 2000) {
+                bonus = salario[i] * 0.1;
+                salarioTotal = salario[i] + bonus;
+            } else {
+                bonus = salario[i] * 0.1;
+                salarioTotal = salario[i] - bonus;
+            }
+
             System.out.println("Bônus: " + bonus);
             System.out.println("\nSalário Líquido: " + salarioTotal);
         }
-        else {
-            double bonus = salario[i] * 0.1;
-            double salarioTotal = salario[i] - bonus;
-            System.out.println("Bônus: " + bonus);
-            System.out.println("\nSalário Líquido: " + salarioTotal);
-        }
-    }
 
+    } catch (Exception e) {
+        System.out.println("Ocorreu um erro inesperado. O programa será encerrado.");
+    } finally {
+        scanner.close();
+    }
 }
-
--- src/Bonus/readme.txt
+++ src/Bonus/readme.txt
@@ -3,3 +3,7 @@ Exercício 3 - Bonus
 Para armazenar o nome e o salário dos funcionários, pensei de modo em fazer 2 arrays separados, de modo que depois quando for calculado o aumento ou desconto utlize apenas o salário.
 Utilizei um for para para ir armazenando os dados na array, até o número de funcionários desejados para cadastrar.
 Para calcular o bônus eu já delimitei em um for conforme for o salário do funcionário para depois calcular o bônus que ele teria.
+Ao fazer o tratamento de exceções mudei um pouco o código de modo que o cliente precise utilizar um valor válido para a quantidade de funcionários (valor inteiro), e digitar também um salário válido (numero e não letra)
+Na parte do cálculo do bônus, também mudei e utilizei variáveis para calcular o valor do bonus e do salário total, o que não estava antes, deixando o código mais enxuto também.
+
+
